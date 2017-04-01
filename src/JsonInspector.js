6165608296c0bbccbfbadade54bbbd2f1aeec2c2
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2017-04-01 20:55:16 +0530

    Add pre script to playground command
    This will build the project before starting the playground.

--- src/JsonInspector.js
+++ src/JsonInspector.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export class JsonInspector extends React.Component {
     render() {
-        return <div>Welcome to JsonInspector</div>;
+        return <div>Happy April Fool!</div>;
     }
 }
